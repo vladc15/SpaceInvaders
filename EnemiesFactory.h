70ac45c7b42a49067e5a63dd45fba2bd1f02fbe0
vladc15 <vciuperceanu@gmail.com>
2023-06-17 00:53:16 +0300

    fix factory

--- EnemiesFactory.h
+++ EnemiesFactory.h
@@ -9,15 +9,19 @@
 
 class EnemiesFactory {
 public:
+    EnemiesFactory() = default;
+    virtual ~EnemiesFactory() = default;
     virtual std::vector<std::shared_ptr<Entity>> createEnemies(sf::Texture& enemyTexture_, sf::Texture& bulletTexture_) = 0;
 };
 
 class EnemyFactory : public EnemiesFactory {
 public:
+    explicit EnemyFactory() = default;
     std::vector<std::shared_ptr<Entity>> createEnemies(sf::Texture& enemyTexture_, sf::Texture& bulletTexture_) override;
 };
 
 class Enemy2Factory : public EnemiesFactory {
 public:
+    explicit Enemy2Factory() = default;
     std::vector<std::shared_ptr<Entity>> createEnemies(sf::Texture& enemyTexture_, sf::Texture& bulletTexture_) override;
 };
